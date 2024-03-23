b3adbbb837997743d72f7a72fa35add710874792
n <feliz.merx@proton.me>
2024-03-23 01:14:30 +0100

    Quick fix on the perspective of the placeholder sofa.

--- state_logic/WalkingState.gd
+++ state_logic/WalkingState.gd
@@ -10,8 +10,12 @@ func _physics_process(delta):
 	if(Input.is_action_pressed("ui_left")):
 		animator.set_animation("walking")
 		animator.flip_h = true
+		actor.velocity=Vector2(-10, 0)
 	elif(Input.is_action_pressed("ui_right")):
+		actor.velocity=Vector2(10, 0)
 		animator.set_animation("walking")
 		animator.flip_h=false
 	else:
 		state_machine.changeState(state_machine.idle_state)
+		actor.velocity = Vector2.ZERO
+	actor.move_and_collide(actor.velocity * delta)
